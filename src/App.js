25eac664b9cad7e2bef40ca1ec69bfcc139213ce
ewspaan <ewspaan@gmail.com>
2020-12-18 18:36:20 +0100

    master ff leeggehaald

--- src/App.js
+++ src/App.js
@@ -1,39 +1,11 @@
-import React,{useState} from 'react';
 import './App.css';
-import PokemonList from "./components/pokemonList/PokemonList";
-import ButtonBar from "./components/buttonBar/ButtonBar";
-import logo from "./assets/Pokemon-Logo-PNG4.png"
-import Button from "./components/button/Button";
 
 function App() {
-    const [upOrDown, setUpOrDown] = useState(0);
-
-    function previous(){
-        if (upOrDown > 0){
-            setUpOrDown(-1)
-        }
-        else {
-            setUpOrDown(upOrDown - 1);
-        }
-    }
-    function next() {
-        if (upOrDown < 0) {
-            setUpOrDown(1)
-        } else {
-            setUpOrDown(upOrDown + 1);
-        }
-    }
-
-  return (
-    <div>
-        <img src={logo} alt="Pokemon Logo"/>
-        <ButtonBar>
-            <Button onClick={previous}>Vorige</Button>
-            <Button onClick={next}>Volgende</Button>
-        </ButtonBar>
-            <PokemonList previousOrNext={upOrDown}/>
-    </div>
-  );
+    return (
+        <div className="App">
+            {/* Hier komt de magie ✨✨✨✨ */}
+        </div>
+    );
 }
 
 export default App;
--- src/components/buttonBar/ButtonBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import "./ButtonBar.css"
-
-function ButtonBar({children}) {
-    return (
-        <div className="buttonbar">{children}</div>)
-}
-export default ButtonBar;
